8f6bb867b9cf430cf043d95d33d79ceba6be90e0
knowcodera <knowcodera@gmail.com>
2024-10-01 15:41:33 -0300

    ajuste

--- main.tf
+++ main.tf
@@ -11,16 +11,15 @@ terraform {
   }
 }
 
+provider "azurerm" {
+  features {}
+}
+
 provider "kubernetes" {
   host                   = azurerm_kubernetes_cluster.aks.kube_config[0].host
   client_certificate     = base64decode(azurerm_kubernetes_cluster.aks.kube_config[0].client_certificate)
   client_key             = base64decode(azurerm_kubernetes_cluster.aks.kube_config[0].client_key)
   cluster_ca_certificate = base64decode(azurerm_kubernetes_cluster.aks.kube_config[0].cluster_ca_certificate)
-}
-
-
-provider "azurerm" {
-  features {}
 }
 
 resource "azurerm_resource_group" "rg" {
